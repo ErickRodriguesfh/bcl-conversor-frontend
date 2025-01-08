d939b689464248d04858fe431f1856f1ed3e86b9
¨Erick <erickrodrigues0150@gmail.com>
2025-01-07 22:32:48 -0300

    ínicio da criação de usuários

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TicketListComponent } from './features/ticket/components/ticket-list/ticket-list.component';
+import { CreateUserComponent } from './features/user/components/create-user/create-user.component';
 
 export const routes: Routes = [
-    {path: 'ticket-list', component: TicketListComponent}
+    {path: 'ticket-list', component: TicketListComponent},
+    {path: 'create-user', component: CreateUserComponent}
 ];
