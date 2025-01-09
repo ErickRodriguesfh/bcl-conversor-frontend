c784e2914838bef237affecd93e8a08abcc3774b
¨Erick <erickrodrigues0150@gmail.com>
2025-01-08 21:48:09 -0300

    visual tela cadastro de usuários

--- src/app/features/user/components/create-user/create-user.component.ts
+++ src/app/features/user/components/create-user/create-user.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-user',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.scss'
 })
 export class CreateUserComponent {
+
+  stepNumber: number;
+  steps = [
+    { title: 'Dados de acesso', isPrimary: true },
+    { title: 'Dados pessoais', isPrimary: false },
+  ];
+
+  constructor() {
+    this.stepNumber = 0;
+  }
+
+  increaseStep() {
+    this.stepNumber++;
+    this.steps.forEach((step, index) => {
+      step.isPrimary = (index <= this.stepNumber);
+    });
+  }
+
+  getClassNames(isPrimary: boolean) {
+    return isPrimary ? 'step step-primary' : 'step';
+  }
 
 }
